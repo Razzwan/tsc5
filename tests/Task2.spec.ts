9bb463fb1c7a16158071678fd8dcddf3d3ee7837
Razzwan <razvanlomov@gmail.com>
2024-01-02 17:24:15 +0300

    first try

--- tests/Task2.spec.ts
+++ tests/Task2.spec.ts
@@ -1,11 +1,17 @@
 import { Blockchain, SandboxContract } from '@ton-community/sandbox';
-import { Cell, toNano } from 'ton-core';
+import {Address, beginCell, Cell, fromNano, toNano} from 'ton-core';
 import { Task2 } from '../wrappers/Task2';
 import '@ton-community/test-utils';
 import { compile } from '@ton-community/blueprint';
+import {TreasuryContract} from '@ton-community/sandbox/dist/treasury/Treasury';
+
+const USER_AMOUNT = 10;
 
 describe('Task2', () => {
     let code: Cell;
+    let admin: SandboxContract<TreasuryContract>;
+    let users: Array<SandboxContract<TreasuryContract>> = [];
+    let users_no_shares: Array<SandboxContract<TreasuryContract>> = [];
 
     beforeAll(async () => {
         code = await compile('Task2');
@@ -16,15 +22,36 @@ describe('Task2', () => {
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
+        admin = await blockchain.treasury('admin');
+        let tons: number = 101;
+        let total_shares: number = 100;
 
-        task2 = blockchain.openContract(Task2.createFromConfig({}, code));
+        let map_entries: Array<[Address, number]> = [];
+        for (let i = 0; i < USER_AMOUNT; i++) {
+            const user = await blockchain.treasury(`user_${i}`);
+            users.push(user);
+            tons += i;
+            total_shares += i;
+            map_entries.push([user.address, i]);
+        }
 
-        const deployer = await blockchain.treasury('deployer');
+        for (let i = 0; i < 10; i++) {
+            users_no_shares.push(await blockchain.treasury(`user_no_shares_${i}`));
+        }
 
-        const deployResult = await task2.sendDeploy(deployer.getSender(), toNano('0.05'));
+        task2 = blockchain.openContract(Task2.createFromConfig({
+            admin_address: admin.address,
+            users: new Map([
+              [admin.address, 100],
+              ...map_entries
+            ]),
+            total: total_shares,
+        }, code));
+
+        const deployResult = await task2.sendDeploy(admin.getSender(), toNano(tons));
 
         expect(deployResult.transactions).toHaveTransaction({
-            from: deployer.address,
+            from: admin.address,
             to: task2.address,
             deploy: true,
             success: true,
@@ -34,5 +61,94 @@ describe('Task2', () => {
     it('should deploy', async () => {
         // the check is done inside beforeEach
         // blockchain and task2 are ready to use
+    });
+
+    it('test dictionary shares getter', async () => {
+        const admin_address = await task2.getAdminAddress();
+        expect(admin_address).toEqualAddress(admin.address);
+
+        const user_shares = await task2.getAddressShares();
+
+        expect(user_shares.get(admin_address.hash)).toEqual(100);
+
+        for (let i = 0; i < USER_AMOUNT; i++) {
+            expect(user_shares.get(users[i].address.hash)).toEqual(i);
+
+            const share_int = await task2.getSharesByAddress(users[i].address);
+            expect(share_int).toEqual(i);
+        }
+
+        for (let i = 0; i < 10; i++) {
+            const share_int = await task2.getSharesByAddress(users_no_shares[i].address);
+            expect(share_int).toEqual(0);
+        }
+    });
+
+    it('add new user with shares', async () => {
+        const add_user = 0x368ddef3;
+        const msg = beginCell()
+          .storeUint(add_user, 32)
+          // query_id - не используется
+          .storeUint(0, 64)
+          .storeAddress(users_no_shares[0].address)
+          .storeUint(200, 32)
+          .endCell();
+        const res = await task2.send(admin.getSender(), toNano('0.05'), msg);
+        expect(res.transactions).toHaveTransaction({
+            from: admin.address,
+            to: task2.address,
+            op: add_user,
+            success: true,
+        });
+
+        const added_user_shares = await task2.getSharesByAddress(users_no_shares[0].address);
+        expect(added_user_shares).toEqual(200);
+    });
+
+    it('update user to add shares', async () => {
+        const add_user = 0x368ddef3;
+
+        const user = users[1];
+
+        const msg = beginCell()
+          .storeUint(add_user, 32)
+          // query_id - не используется
+          .storeUint(0, 64)
+          .storeAddress(user.address)
+          .storeUint(200, 32)
+          .endCell();
+        const res = await task2.send(admin.getSender(), toNano('0.05'), msg);
+        expect(res.transactions).toHaveTransaction({
+            from: admin.address,
+            to: task2.address,
+            op: add_user,
+            success: true,
+        });
+
+        const updated_user_shares = await task2.getSharesByAddress(user.address);
+        expect(updated_user_shares).toEqual(201);
+    });
+
+    it('remove user from shares list', async () => {
+        const remove_user = 0x278205c8;
+
+        const user = users[1];
+
+        const msg = beginCell()
+          .storeUint(remove_user, 32)
+          // query_id - не используется
+          .storeUint(0, 64)
+          .storeAddress(user.address)
+          .endCell();
+        const res = await task2.send(admin.getSender(), toNano('0.05'), msg);
+        expect(res.transactions).toHaveTransaction({
+            from: admin.address,
+            to: task2.address,
+            op: remove_user,
+            success: true,
+        });
+
+        const updated_user_shares = await task2.getSharesByAddress(user.address);
+        expect(updated_user_shares).toEqual(0);
     });
 });
--- wrappers/Task2.ts
+++ wrappers/Task2.ts
@@ -1,9 +1,31 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
+import {
+    Address,
+    beginCell,
+    Cell,
+    Contract,
+    contractAddress,
+    ContractProvider,
+    Dictionary,
+    Sender,
+    SendMode, TupleItemSlice
+} from 'ton-core';
 
-export type Task2Config = {};
+export type Task2Config = {
+    admin_address: Address,
+    users: Map<Address, number>,
+    total: number,
+};
 
 export function task2ConfigToCell(config: Task2Config): Cell {
-    return beginCell().endCell();
+    const userShares = Dictionary.empty(Dictionary.Keys.Buffer(32), Dictionary.Values.Uint(32));
+    config.users.forEach((v, key) => {
+        userShares.set(key.hash, v);
+    });
+    return beginCell()
+      .storeAddress(config.admin_address)
+      .storeDict(userShares, Dictionary.Keys.Buffer(32), Dictionary.Values.Uint(32))
+      .storeUint(config.total, 32)
+      .endCell();
 }
 
 export class Task2 implements Contract {
@@ -25,5 +47,32 @@ export class Task2 implements Contract {
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
         });
+    }
+
+    async send(provider: ContractProvider, via: Sender, value: bigint, body: Cell) {
+        await provider.internal(via, {
+            value,
+            body,
+        });
+    }
+
+    async getAddressShares(provider: ContractProvider): Promise<Dictionary<Buffer, number>> {
+        const result = await provider.get('get_address_shares', []);
+        return Dictionary.loadDirect(
+          Dictionary.Keys.Buffer(32),
+          Dictionary.Values.Uint(32),
+          result.stack.readCell()
+        );
+    }
+
+    async getSharesByAddress(provider: ContractProvider, address: Address): Promise<number> {
+        const options: [TupleItemSlice] = [{type: 'slice', cell: beginCell().storeAddress(address).endCell()}];
+        const result = await provider.get('get_shares_by_address', options);
+        return result.stack.readNumber();
+    }
+
+    async getAdminAddress(provider: ContractProvider): Promise<Address> {
+        const result = await provider.get('get_admin_address', []);
+        return result.stack.readAddress();
     }
 }
